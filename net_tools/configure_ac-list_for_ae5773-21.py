85cd90c46a7fb72daf6a1514907102c7c2efd21b
Keen <keenchen63@outlook.com>
2024-07-17 17:50:42 +0800

    Changes to be committed: 	modified:   configure_ac-list_for_ae5773-21.py

--- net_tools/configure_ac-list_for_ae5773-21.py
+++ net_tools/configure_ac-list_for_ae5773-21.py
@@ -1,5 +1,4 @@
 import paramiko
-import openpyxl
 from concurrent.futures import ThreadPoolExecutor
 import re
 import time
@@ -16,7 +15,6 @@ def ssh_connect(hostname, username, password):
 def execute_and_print(channel, command):
     channel.send(command + '\n')
     time.sleep(2)  # 等待命令执行
-    
     # 使用 recv 方法持续接收数据，直到命令执行完成
     output = ""
     while True:
@@ -24,66 +22,31 @@ def execute_and_print(channel, command):
             output += channel.recv(1024).decode('utf-8')
         if re.search(r'>|\$', output):  # 检查命令提示符，表示命令执行完成
             break
-        time.sleep(0.1)
-    
+        time.sleep(0.1)    
     print(f"Command: {command}\nOutput: {output}")
     return output
 
 # 设置新密码的函数
 def set_new_password(ssh, new_password):
     channel = ssh.invoke_shell()
-    time.sleep(1)
-    
+    time.sleep(1)    
     # 读取初始输出
     output = channel.recv(9999).decode('utf-8')
-    print(output)
-    
+    print(output)    
      # 发送新密码
-    output = execute_and_print(channel, new_password + '\n')
-        
+    output = execute_and_print(channel, new_password + '\n')        
     # 发送确认密码
-    output = execute_and_print(channel, new_password + '\n')
-    
+    output = execute_and_print(channel, new_password + '\n')    
     return output
 
-# 获取设备SN号的函数
-def get_sn(channel):
-    channel.send("display system/system-info/esn\n")
-    output = execute_and_print(channel, "display system/system-info/esn\n")
-    
-    sn_match = re.search(r'(215[0-9A-Za-z]{17})', output)
-
-    if sn_match:
-        print(f"The device SN is {sn_match}")
-        return sn_match.group(1)
-    else:
-        print("SN not found in output")  # 打印未找到SN的调试信息
-    return None
-
-# 根据SN号在Excel文件中查找对应的IP地址
-def find_ip_for_sn(sn, excel_file):
-    workbook = openpyxl.load_workbook(excel_file)
-    sheet = workbook.active
-    for row in sheet.iter_rows(min_row=2, values_only=True):  # 假设第一行是标题
-        sn_in_file, ip, gateway, mask = row
-        if sn_in_file.strip().lower() == sn.strip().lower():
-            return ip, gateway, mask
-    return None, None, None
-
-# 配置设备IP地址的函数
-def configure_ip_address(channel, ip_address, ip_gateway, ip_mask):
-    # commands = [
-    #     "system-view",
-    #     "interface GE 0/0/1",
-    #     f"ip address {ip_address} 24",
-    #     "quit",
-    #     "quit",
-    #     "save",
-    #     "yes"
-    # ]
-    # for command in commands:
-    #     execute_and_print(channel, command)
-    print(f"The device IP is {ip_address} / {ip_gateway} / {ip_mask}")
+# 配置设备ac-list的函数
+def configure_aclist_address(channel, ac_ip_address):
+    commands = [
+        "diagnose apaddress ac-ipv4-list \n"
+        f"ac-ip-list {ac_ip_address} \n"
+    ]
+    for command in commands:        
+        execute_and_print(channel, command)
     return None
 
 def save_config(channel):
@@ -96,48 +59,22 @@ def save_config(channel):
     return None
 
 # 处理单个设备的函数
-def process_device(ip, username, initial_password, new_password, excel_file):
+def process_device(ip, username, initial_password, new_password, ac_ip_address):
     try:
         try:
             # 第一次使用初始密码连接
             ssh = ssh_connect(ip, username, initial_password)
-            
             # 设置新密码
             print(f"Setting new password for device {ip}")
-            set_new_password(ssh, new_password)
-            
+            set_new_password(ssh, new_password)            
             # 由于设备会自动断开连接，等待一段时间再重新连接
             time.sleep(8)
         except:
-
             #使用新密码重新连接
             ssh = ssh_connect(ip, username, new_password)
-            channel = ssh.invoke_shell()
-            
-            # 获取设备的SN号
-            sn = get_sn(channel)
-            if sn:
-                print(f"Device {ip} SN: {sn}")
-                ip_address, ip_gateway, ip_mask = find_ip_for_sn(sn, excel_file)
-                if ip_address:
-                    print(f"Configuring IP Address: {ip_address} / {ip_gateway} / {ip_mask} for device {ip}")
-                    configure_ip_address(channel, ip_address, ip_gateway, ip_mask)
-                    ssh.close()
-
-                    try:
-                            
-                        ssh = ssh_connect(ip_address, username, new_password)
-                        channel = ssh.invoke_shell()
-
-                        save_config(channel)
-                    except Exception as e:
-                        print(f"Failed to save config {ip}: {e}")
-
-                else:
-                    print(f"SN {sn} not found in Excel file for device {ip}.")
-            else:
-                print(f"Failed to get SN for device {ip}")
-            
+            channel = ssh.invoke_shell()            
+            configure_aclist_address(channel, ac_ip_address)            
+            save_config(channel)
             ssh.close()
     except Exception as e:
         print(f"Failed to process device {ip}: {e}")
@@ -147,14 +84,13 @@ def main():
     username = "admin"
     initial_password = "admin@huawei.com"
     new_password = "Huawei@123"
-    excel_file = "sn_ip_mapping.xlsx"
     ip_subnet = ipaddress.ip_network('192.168.1.196/32')
-
+    ac_ip_address = "172.31.161.240"
 
     with ThreadPoolExecutor(max_workers=10) as executor:  # 根据需要调整工作线程数
         for i in ip_subnet:
             ip = f"{i}"
-            executor.submit(process_device, ip, username, initial_password, new_password, excel_file)
+            executor.submit(process_device, ip, username, initial_password, new_password, ac_ip_address)
 
 if __name__ == "__main__":
     main()
